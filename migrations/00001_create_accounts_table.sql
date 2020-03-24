556db30596b92cb8bf52c56a360b589a26b11f95
Nikita Savik <nikita@liveplanet.net>
2020-03-24 16:11:23 +0300

    billing: Add migrations

---
+++ migrations/00001_create_accounts_table.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+CREATE TABLE IF NOT EXISTS `billing_accounts` (
+  `id`         VARCHAR(255) NOT NULL,
+  `user_id`    VARCHAR(255) NOT NULL,
+  `created_at` TIMESTAMP DEFAULT NOW(),
+  `updated_at` TIMESTAMP NULL DEFAULT NULL,
+  `balance`    INT(20) DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+DROP TABLE billing_accounts;
