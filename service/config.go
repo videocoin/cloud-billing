883ccc1a9579bd949c3cf90b610cb7558fa394b9
Nikita Savik <nikita@liveplanet.net>
2020-03-26 17:41:56 +0300

    Change dburi by default

--- service/config.go
+++ service/config.go
@@ -12,7 +12,7 @@ type Config struct {
 	RPCAddr               string `envconfig:"RPC_ADDR" default:"0.0.0.0:5020"`
 	UsersRPCAddr          string `envconfig:"USERS_RPC_ADDR" default:"0.0.0.0:5000"`
 	AccountsRPCAddr       string `envconfig:"ACCOUNTS_RPC_ADDR" default:"0.0.0.0:5001"`
-	DBURI                 string `envconfig:"DBURI" default:"root:@/cloud?charset=utf8&parseTime=True&loc=Local"`
+	DBURI                 string `envconfig:"DBURI" default:"root:@/videocoin?charset=utf8&parseTime=True&loc=Local"`
 	MQURI                 string `envconfig:"MQURI" default:"amqp://guest:guest@127.0.0.1:5672"`
 	AuthTokenSecret       string `envconfig:"AUTH_TOKEN_SECRET" default:"secret"`
 	StripeKey             string `envconfig:"STRIPE_KEY" required:"true"`
