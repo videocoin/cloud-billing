17244ae37e0139a19a169a37d35232fbea733aad
Aleksandr Didenko <aa.didenko@yandex.com>
2020-04-06 14:10:11 +0300

    Increase timeouts to background tasks

--- manager/manager.go
+++ manager/manager.go
@@ -21,8 +21,8 @@ type Manager struct {
 
 func New(opts ...Option) (*Manager, error) {
 	ds := &Manager{
-		cpsticker: time.NewTicker(time.Second * 5),
-		uatticker: time.NewTicker(time.Second * 30),
+		cpsticker: time.NewTicker(time.Second * 60 * 5),
+		uatticker: time.NewTicker(time.Second * 60),
 	}
 	for _, o := range opts {
 		if err := o(ds); err != nil {
